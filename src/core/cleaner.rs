8c9ea39d6ed24478cfcf40e2eac43ae8cae803f8
QuocAn108 <anhoang800140@gmail.com>
2026-09-10 15:45:14 +0700

    feat(core): add core module exports and cleaner placeholder

---
+++ src/core/cleaner.rs
@@ -0,0 +1,16 @@
+﻿use std::path::Path;
+use anyhow::Result;
+
+#[allow(dead_code)]
+pub struct Cleaner;
+
+#[allow(dead_code)]
+impl Cleaner {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn delete_artifact(&self, _path: &Path) -> Result<()> {
+        todo!("Cleaner will be implemented in Sprint 3");
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,4 @@
-﻿pub mod engine;
+﻿pub mod cleaner;
+pub mod engine;
 pub mod size;
 pub mod traits;
